9c7b89a073a908b1c5011278b8df938545ecff45
Kendall Duran <119521302+kendalljdv@users.noreply.github.com>
2024-04-22 18:52:50 -0600

    small changes

--- TravelingAgency/src/main/java/com/traveling/travelingagency/WebConfig.java
+++ TravelingAgency/src/main/java/com/traveling/travelingagency/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Permite solicitudes de origen cruzado desde este origen
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
-                .allowedHeaders("*") // Permite todas las cabeceras
-                .allowCredentials(true); // Permite credenciales
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
